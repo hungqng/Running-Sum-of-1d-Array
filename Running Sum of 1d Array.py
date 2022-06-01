44b338271fb8502d537af7a1731eddefb8065288
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-31 22:21:36 -0700

    Add files via upload

--- Running Sum of 1d Array.py
+++ Running Sum of 1d Array.py
@@ -12,3 +12,10 @@ class Solution:
             total += nums[i]
             result.append(total)
         return result
+
+        # Solution 2
+        result = []
+        result.append(nums[0])
+        for i in range(1, len(nums)):
+            result.append(result[-1]+nums[i])
+        return result
